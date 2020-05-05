18534317562ad527a2174faae403b39f2549a1bf
marcinolech <mrc.olech@gmail.com>
2020-05-05 10:16:16 +0200

    initial commit, works

---
+++ src/main/java/olech/marcin/view/FXMLController.java
@@ -0,0 +1,45 @@
+package olech.marcin.view;
+
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+import olech.marcin.model.Model;
+import olech.marcin.model.Pattern;
+
+public class FXMLController implements Initializable {
+    @FXML
+    private Canvas cellCanvas;
+    @FXML
+    private Label currentPattern;
+
+    /**
+     * Updates label with given starting pattern
+     * @param currentStartingPattern value of Pattern enum
+     */
+    public void updateCurrentPattern(Pattern currentStartingPattern) {
+        currentPattern.setText(currentStartingPattern.name());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<Pattern> observableList = FXCollections.observableArrayList();
+        observableList.addAll(Pattern.values());
+
+        cellCanvas.setHeight(Model.BOARD_HEIGHT);
+        cellCanvas.setWidth(Model.BOARD_WIDTH);
+    }
+
+    public Canvas getCellCanvas() {
+        return cellCanvas;
+    }
+
+}
